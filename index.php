c3c82b667681c395ccb88f1d2c44a93b1cdd15a8
Martín Reyes <rreyes1700@gmail.com>
2025-06-30 23:15:29 -0300

    Actualizando el acceso inicials

--- index.php
+++ index.php
@@ -17,53 +17,58 @@ if ($_SESSION){
         <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
               integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="menu/style.css">
     </head>
     <body>
         <section id="login">
             <div class="container-fluid">
                 <div class="row vh-100">
-            <div class="container well" id="sha">
-                <div class="col-5 d-none d-lg-block position-relative">
-                    <div class="bg-image"></div>
-                </div>
-                <div class="col-sm-10 col-lg-6 px-md-5 align-self-center mx-auto">
-                    <div class="text-center mb-4">
-                        <h6>Inicia sesión con tu cuenta</h6>
-                    </div>
-                    <div class="card border-0 round-3 px-md-5 py-2">
-                        <div class="card-header bg-white border-bottom-0 text-center">
-                                <img src="img/Hospital.png" alt="">
+                        <div class="col-5 d-none d-lg-block position-relative">
+                            <div class="bg-image"></div>
+                        </div>
+                        <div class="col-sm-10 col-lg-6 px-md-5 align-self-center mx-auto">
+                            <div class="text-center mb-4">
+                                <h6>Inicia sesión con tu cuenta</h6>
                             </div>
-            <form class="acceso-usuario" action="acceso.php" method="post">
-                <div class="form-group has-feedback">
-                    <input type="text" class="form-control" name="usuario" required="" autofocus="" placeholder="Usuario"/>
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                            <div class="card border-0 round-3 px-md-5 py-2">
+                                <div class="card-header bg-white border-bottom-0 text-center">
+                                    <img src="img/Hospital.png" alt="">
+                                </div>
+                                <div class="card-body pt-0">
+                                    <form action="acceso.php" method="post" class="row g-3">
+                                        <div class="col-md-12">
+                                            <label for="Usuario" class="form-label">Usuario</label>
+                                            <span class="text-danger">*</span>
+                                            <input type="email" class="form-control rounded-0" name="usuario" placeholder="Ingrese su usuario"/>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <label for="Password" class="form-label">Contraseña</label>
+                                            <span class="text-danger">*</span>
+                                            <input type="password" class="form-control rounded-0" name="contraseña" aria-describedby="passwordHelpBlock" placeholder="Ingresa tu contraseña"/>
+                                        </div>
+                                        <div class="d-flex justify-content-between">
+                                            <a href="#" class="f-password">
+                                                <u>¿Olvidaste tu contraseña</u>
+                                            </a>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <button class="btn border-0" type="submit">Acceder</button>
+                                        </div>
+                                            <?php
+                                            if (!empty($_SESSION['error'])){?>
+                                        <div class="alert alert-danger" role="alert">
+                                            <?php echo $_SESSION['error']; ?>
+                                        </div>
+                                            <?php }?>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                 </div>
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" name="contraseña" required="" placeholder="Contraseña"/>
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesión</button>
-                <div class="checkbox" style="text-align: center;">
-                    <label class="checkbox">
-                        <input type="checkbox" value="1" name="recordarsesion"/>Recordar mi sesion
-                    </label>
-                    <p class="help-block"><a href="/Agendamiento2024/recuperar_index.php">¿Olvidaste tu contraseña?</a></p>
-                </div>
-                <?php
-                if (!empty($_SESSION['error'])){?>
-                <div class="alert alert-danger" role="alert">
-                    <span class="glyphicon glyphicon-exclamation-sign"></span>
-                    <?php echo $_SESSION['error']; ?>
-                    </div>
-                <?php }?>
-            </form>
-        </div>
-                    </div>
-                    </div>
-                    </div>
-                </div>
+            </div>
         </section>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <script src="js/jquery-1.12.2.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
     </body>
