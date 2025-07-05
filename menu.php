93b6aa39eea47bf1352f785beba7e83aaa9917f4
Martín Reyes <rreyes1700@gmail.com>
2025-07-05 07:26:15 -0300

    Actualizando el proyecto

--- menu.php
+++ menu.php
@@ -3,20 +3,20 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <title>Menú Principal</title>
         <link rel="shortcut icon" href="img/icono.ico" type="image/x-icon">
-        <link rel="stylesheet" href="menu/style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
             <?php
             session_start();
             require 'menu/css_lte.ctp'
             ?>
     </head>
-    <body class="p-3 m-0 border-0 bd-example m-0 border-0">
+    <body class="hold-transition skin-blue sidebar-mini">
         <div class="wrapper">
-            
             <?php require 'menu/toolbar_lte.ctp';?>
             <div class="content-wrapper">
+                
             </div>
             <?php require 'menu/footer_lte.ctp'; ?>
         </div>                  
